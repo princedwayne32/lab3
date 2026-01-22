bbb136b9b0ff30be1d58a7d3cdc7765380184132
Bautista, Prince Dwayne C. <mpdbautista1@tip.edu.ph>
2026-01-22 18:22:39 +0800

    Add UserAPI component

--- vite-project/src/UserAPI.js
+++ vite-project/src/UserAPI.js
@@ -1,22 +1,29 @@
 import React, { useState, useEffect } from 'react';
 
 function UserAPI() {
-    const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState([]);
 
-    useEffect(() => {
-        async function fetchUsers() {
-            const res = await fetch('https://jsonplaceholder.typicode.com/users');
-            const data = await res.json();
-            setUsers(data);
-        }
-        fetchUsers();
-    }, []);
+  useEffect(() => {
+    async function fetchUsers() {
+      try {
+        const res = await fetch('https://jsonplaceholder.typicode.com/users');
+        const data = await res.json();
+        setUsers(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+    fetchUsers();
+  }, []);
 
-    return (
-        <ul>
-            {users.map(user => <li key={user.id}>{user.name}</li>)}
-        </ul>
-    );
+  return (
+    <div>
+      <h3>User API Data</h3>
+      <ul>
+        {users.map(user => <li key={user.id}>{user.name}</li>)}
+      </ul>
+    </div>
+  );
 }
 
 export default UserAPI;
