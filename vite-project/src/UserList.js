efef5b917b086bb1bad71929aa88fe588735e00a
Bautista, Prince Dwayne C. <mpdbautista1@tip.edu.ph>
2026-01-22 18:20:55 +0800

    Add UserList component

--- vite-project/src/UserList.js
+++ vite-project/src/UserList.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 function UserList({ users }) {
-    return (
-        <ul>
-            {users.map(user => <li key={user}>{user}</li>)}
-        </ul>
-    );
+  return (
+    <div>
+      <h3>User List Component</h3>
+      <ul>
+        {users.map(user => <li key={user}>{user}</li>)}
+      </ul>
+    </div>
+  );
 }
 
 export default UserList;
