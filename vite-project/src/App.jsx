0a580ab2b83fe31b2653321b369962fbbf0c244f
Bautista, Prince Dwayne C. <mpdbautista1@tip.edu.ph>
2026-01-22 18:29:29 +0800

    Add Counter component

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,19 +1,15 @@
 import React from 'react';
 import UserList from './UserList';
-import Counter from './Counter';
-import UserAPI from './UserAPI'; // Import API component
+import Counter from './Counter'; // Import Counter
 
 function App() {
   const users = ['Alice', 'Bob', 'Charlie'];
-  const isLoggedIn = true; 
 
   return (
     <div>
       <h1>Lab Activity 3</h1>
-      {isLoggedIn ? <p>Welcome back!</p> : <p>Please log in.</p>}
       <UserList users={users} />
       <Counter />
-      <UserAPI />
     </div>
   );
 }
